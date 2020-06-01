b4c73e10a30280ad367cfeef964df2dbc0b29664
Riccardo Schirone <sirmy15@gmail.com>
2020-06-01 16:03:07 +0200

    Hopefully

--- shlr/radare2-shell-parser-deps.mk
+++ shlr/radare2-shell-parser-deps.mk
@@ -1,6 +1,7 @@
 LINK+=$(STOP)/radare2-shell-parser/libshell-parser.$(EXT_AR)
 ifeq ($(USE_LIB_TREESITTER),1)
 LINK+=$(LIBTREE_SITTER)
+LDFLAGS+=${LIBTREE_SITTER}
 else
 LINK+=$(STOP)/tree-sitter/libtree-sitter.$(EXT_AR)
 endif
