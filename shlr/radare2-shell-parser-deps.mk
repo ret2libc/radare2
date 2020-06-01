0464211f1228b978b51c20f11db70b05bd93262a
Riccardo Schirone <sirmy15@gmail.com>
2020-06-01 15:44:34 +0200

    Use sys lib tree-sitter when configured so

--- shlr/radare2-shell-parser-deps.mk
+++ shlr/radare2-shell-parser-deps.mk
@@ -1,2 +1,6 @@
 LINK+=$(STOP)/radare2-shell-parser/libshell-parser.$(EXT_AR)
+ifeq ($(USE_LIB_TREESITTER),1)
+LINK+=$(LIBTREE_SITTER)
+else
 LINK+=$(STOP)/tree-sitter/libtree-sitter.$(EXT_AR)
+endif
