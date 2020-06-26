d7ddf07d4200c29c0d0bf61bbdf2fd67311fe734
Riccardo Schirone <sirmy15@gmail.com>
2020-06-26 14:51:29 +0200

    test 5

--- .github/vsdevenv.bat
+++ .github/vsdevenv.bat
@@ -10,6 +10,8 @@ for /f "usebackq delims=*" %%i in (`%VSWHERE% -latest -property installationPath
 :: Loop over all environment variables and make them global using set-env.
 :: See: https://help.github.com/en/articles/development-tools-for-github-actions#set-an-environment-variable-set-env
 :: See: https://stackoverflow.com/questions/39183272/loop-through-all-environmental-variables-and-take-actions-depending-on-prefix
+setlocal
 for /f "delims== tokens=1,2" %%a in ('set') do (
   echo ::set-env name=%%a::%%b
 )
+endlocal
