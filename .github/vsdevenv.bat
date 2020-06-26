54bb20784e6b4a23f6a9dd08e19c8d089b69a7d1
Riccardo Schirone <sirmy15@gmail.com>
2020-06-26 13:54:00 +0200

    do not use backend

--- .github/vsdevenv.bat
+++ .github/vsdevenv.bat
@@ -1,11 +1,10 @@
-:: @echo off
+@echo off
 
 SET VSWHERE="C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere"
 
 :: See https://github.com/microsoft/vswhere/wiki/Find-VC
 for /f "usebackq delims=*" %%i in (`%VSWHERE% -latest -property installationPath`) do (
   call "%%i"\Common7\Tools\vsdevcmd.bat %*
-  echo "test"
 )
 
 :: Loop over all environment variables and make them global using set-env.
